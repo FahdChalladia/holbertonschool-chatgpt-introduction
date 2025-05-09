9f65fcf7f9d7703ae59279fa4df3f766b0c8c979
fahdchalladia <fahdchalladia2004@gmail.com>
2025-05-09 09:54:17 +0100

    new version

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -9,4 +9,15 @@ def factorial(n):
     return result
 
 f = factorial(int(sys.argv[1]))
+print(f)#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+f = factorial(int(sys.argv[1]))
 print(f)
